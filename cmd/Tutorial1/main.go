e1427817a7bf84e0988285569ca8a5c56f2e3199
BIT_LOW <prajwalsharma093@gmail.com>
2025-03-24 13:53:39 +1100

    Go

---
+++ cmd/Tutorial1/main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func factorial(a int) int {
+	if a == 0 {
+		return 1
+	} else {
+		return a * factorial(a-1)
+	}
+}
+
+func main() {
+
+	var fact int = factorial(5)
+
+	fmt.Printf("the factorial of 5 is %d\n", fact)
+
+}
